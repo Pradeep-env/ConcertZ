53451b372c97404f9061b9f5e96b71db2bc8bde6
Pradeep-env <pradeepkh312@gmail.com>
2026-07-07 17:34:00 +0530

    notifications, support, chatroom and chats table commands

--- db/psql.sql
+++ db/psql.sql
@@ -15,3 +15,13 @@ create table attendee_tickets (booking_id uuid primary key, user_id uuid, ticket
 create table projects (project_id uuid primary key, concert_id uuid, status boolean default true, details TEXT, created_on timestamp default now(), expires_on timestamp, foreign key(concert_id) references concerts(concert_id));
 
 create table team (teamate_id uuid primary key, project_id uuid, employee_id uuid, status boolean default true, joined_on timestamp default now(), duration int, foreign key(project_id) references projects(project_id), foreign key(employee_id) references employee_profile(employee_id));
+
+create table notifications (noti_id uuid primary key, user_id uuid, message TEXT, time timestamp default now(), status boolean default false, foreign key(user_id) references users(id));
+
+create table support_tickets(ticket_id uuid primary key, user_id uuid, project_id uuid, query TEXT, raised_date timestamp default now(), close_date timestamp default null, status boolean default false, foreign key(user_id) references users(id), foreign key(project_id) references projects(project_id));
+
+create table tasks(task_id uuid primary key, employee_id uuid, project_id uuid, description TEXT, start_date timestamp default now(), close_date timestamp default null, status boolean default false, foreign key(employee_id) references employee_profile(employee_id), foreign key(project_id) references projects(project_id));
+
+create table chatrooms(chatroom_id uuid primary key, room_name TEXT, project_id uuid, description TEXT, start_date timestamp default now(), status boolean default false, foreign key(project_id) references projects(project_id)); 
+
+create table chats(chat_id uuid primary key, user_id uuid, chatroom_id uuid,chat_type varchar(1) check(chat_type in ('t', 'a', 'v', 'f')) , message TEXT default null, file bytea default null, sent_date timestamp default now(), reply_id uuid default null, status boolean default false, foreign key(user_id) references users(id), foreign key(chatroom_id) references chatrooms(chatroom_id));
